3f8d3932404aa79406dd9d918ca1ed84d2bac839
ivykibe <maleeivy@gmail.com>
2023-09-29 16:57:51 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function addingEventListener(){
+    const input = doument.getElementById('input');
+
+    function clickAlert(){
+    }
+    input.addingEventListener('click',clickAlert);
+}
+
+//Call the addingEventListener function to set up the event listener
+addingEventListener();
